c4383e20bef95d80f7163c1736ad109ea73a8d42
Mercy-12 <mehretmeridt@gmail.com>
2022-03-11 11:07:51 -0800

    comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,11 +9,14 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar ((num % 10) + '0');
-		if (num < 10)
-			putchar(',');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
 	putchar ('\n');
 	return (0);
