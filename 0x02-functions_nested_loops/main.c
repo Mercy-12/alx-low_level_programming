0f2ce55e5228db45d56eed8496d1d7214e2698a1
Mercy-12 <mehretmeridt@gmail.com>
2022-03-15 09:55:24 -0700

    0 putchar

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *c = "_putchar";
+
+    while(*c != '\0')
+    {
+	    _putchar(*c);
+    c++;
+    }
+    _putchar('\n');
+    return (0);
+}
