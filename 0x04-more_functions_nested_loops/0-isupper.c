a5d17a0d9bfabffaf5b1e7b7bbd1d1bb315b91bf
Mercy-12 <mehretmeridt@gmail.com>
2022-03-17 08:28:03 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - prints if the character is upper 1 otherwise 0
+ * @c: character to check the uppercase
+ * Return: 0 or 1
+ **/
+
+int _isupper(int c)
+{
+
+	return (c >= 65 && c <= 89);
+
+}
