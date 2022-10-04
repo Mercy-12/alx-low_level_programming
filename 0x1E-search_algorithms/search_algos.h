5c6ddd8bd0f7b8e1df30d4502a9b4d82b5b2a39f
Mercy-12 <mehretmeridt@gmail.com>
2022-10-04 00:44:29 -0700

    upload initial repository

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,11 +1,16 @@
-#ifndef SEARCH_ALGO_H
-#define SEARCH_ALGO_H
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
 
-#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *a, int i, int j);
+int help_binary(int *array, int value, size_t lo, size_t hi);
+void array_print(int *array, size_t lo, size_t hi);
+int jump_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
